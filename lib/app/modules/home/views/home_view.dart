33e37523a4ddbdb3e020feadcaff475df20296eb
Sahib <chyabbas34@gmail.com>
2025-05-01 23:56:06 +0600

    Added BottomNavigationBar to Home with tab switching for Home and Notifications

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,24 +1,62 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({super.key});
+  const HomeView({Key? key}) : super(key: key);
+
+  static const List<Widget> _pages = [
+    Center(child: Text('Home Page Placeholder', style: TextStyle(fontSize: 20))),
+    Center(child: Text('No Notifications', style: TextStyle(fontSize: 20))),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+    return Obx(() => Scaffold(
+          appBar: AppBar(
+            title: const Text('Digi Shop'),
+            centerTitle: true,
+          ),
+          body: _pages[controller.selectedIndex.value],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: controller.selectedIndex.value,
+            onTap: controller.changeTabIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.notifications),
+                label: 'Notifications',
+              ),
+            ],
+          ),
+        ));
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// import 'package:get/get.dart';
+
+// import '../controllers/home_controller.dart';
+
+// class HomeView extends GetView<HomeController> {
+//   const HomeView({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('HomeView'),
+//         centerTitle: true,
+//       ),
+//       body: const Center(
+//         child: Text(
+//           'HomeView is working',
+//           style: TextStyle(fontSize: 20),
+//         ),
+//       ),
+//     );
+//   }
+// }
